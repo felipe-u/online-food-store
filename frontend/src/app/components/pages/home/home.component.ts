4eb9ed9a05e1e5c8531a5419b2507c2f4598a570
felipe-u <felipe_uv@outlook.com>
2024-05-18 20:50:57 -0500

    Adding Search Feature

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Food } from '../../../shared/models/food.model';
 import { FoodService } from '../../../services/food.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
   foods: Food[] = [];
 
-  constructor(private foodService: FoodService) {}
+  constructor(private foodService: FoodService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.foods = this.foodService.getAll();
+    this.route.params.subscribe((params => {
+      if (params.searchTerm)
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm)
+      else
+        this.foods = this.foodService.getAll();
+    }));
   }
 }

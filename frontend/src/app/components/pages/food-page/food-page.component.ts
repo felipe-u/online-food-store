50ec88ec467aef9bf59d5fac0b45f4790097bbc7
felipe-u <felipe_uv@outlook.com>
2024-05-18 23:24:09 -0500

    Creating Cart Service

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Food } from '../../../shared/models/food.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FoodService } from '../../../services/food.service';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -11,12 +12,21 @@ import { FoodService } from '../../../services/food.service';
 export class FoodPageComponent implements OnInit {
   food!: Food;
 
-  constructor(private route: ActivatedRoute, private foodService: FoodService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private foodService: FoodService,
+    private cartService: CartService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params => {
       if (params.id)
         this.food = this.foodService.getFoodById(params.id)
     }));
+  }
+
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page')
   }
 }

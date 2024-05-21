f375d52f70937d9c8d8545e1eac0bda79d67f4a5
felipe-u <felipe_uv@outlook.com>
2024-05-20 23:54:02 -0500

    Set Dynamic Cart Option In Header

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  cartQuantity = 0;
 
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cartQuantity = cart.totalCount;
+    });
+  }
 }

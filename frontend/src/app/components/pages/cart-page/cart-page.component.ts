4c530cba84bec7a481948f7025b518e4296588a8
felipe-u <felipe_uv@outlook.com>
2024-05-20 23:53:38 -0500

    Improving CartPage

--- frontend/src/app/components/pages/cart-page/cart-page.component.ts
+++ frontend/src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cart } from '../../../shared/models/cart.model';
+import { CartService } from '../../../services/cart.service';
+import { CartItem } from '../../../shared/models/cart-item.model';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
+  cart!: Cart;
 
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    })
+  }
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+  }
 }

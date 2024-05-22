b659e871df193069840e62e10e0efcf1abc2a0d5
felipe-u <felipe_uv@outlook.com>
2024-05-22 17:07:57 -0500

    Adding LocalStorage Support and Implementing Logout

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../../services/cart.service';
+import { UserService } from '../../../services/user.service';
+import { User } from '../../../shared/models/user.model';
 
 @Component({
   selector: 'app-header',
@@ -8,12 +10,25 @@ import { CartService } from '../../../services/cart.service';
 })
 export class HeaderComponent implements OnInit {
   cartQuantity = 0;
+  user!: User;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.cartService.getCartObservable().subscribe((cart) => {
       this.cartQuantity = cart.totalCount;
     });
+
+    this.userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
+    });
+  }
+
+  logout() {
+    this.userService.logout();
+  }
+
+  get isAuth() {
+    return this.user.token;
   }
 }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -6,11 +6,13 @@ import { HttpClient } from '@angular/common/http';
 import { USER_LOGIN_URL } from '../shared/constants/urls';
 import { ToastrService } from 'ngx-toastr';
 
+const USER_KEY = 'User';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private userSubject = new BehaviorSubject<User>(new User());
+  private userSubject = new BehaviorSubject<User>(this.getUserFromLocalStorage());
   public userObservable: Observable<User>;
 
   constructor(private http: HttpClient, private toastrService: ToastrService) {
@@ -21,6 +23,7 @@ export class UserService {
     return this.http.post<User>(USER_LOGIN_URL, userLogin)
       .pipe(tap({
         next: (user) => {
+          this.setUserToLocalStorage(user);
           this.userSubject.next(user);
           this.toastrService.success(
             `Welcome to Foodmin ${user.name}`,
@@ -31,5 +34,21 @@ export class UserService {
           this.toastrService.error(errorResponse.error, 'Login Failed');
         }
       }));
+  }
+
+  logout() {
+    this.userSubject.next(new User());
+    localStorage.removeItem(USER_KEY);
+    window.location.reload();
+  }
+
+  private setUserToLocalStorage(user: User) {
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  private getUserFromLocalStorage(): User {
+    const userJSON = localStorage.getItem(USER_KEY);
+    if (userJSON) return JSON.parse(userJSON) as User;
+    return new User();
   }
 }

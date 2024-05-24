4bdceb392b3bd3fa1176d681cb27f6ff25bdb37d
felipe-u <felipe_uv@outlook.com>
2024-05-23 21:05:10 -0500

    MongoDB Setup: Connecting App with MongoDB Atlas

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Just for fun, and obviously for practicing skills.
+
+// Mongo DB Process
+Connect Login API to MongoDB Atlas
+1. Moving Apis into routers
+2. Create MongoDB Atlas
+3. Create .env file
+4. Install
+    1. mongoose: for a better MongoDB control
+    2. dotenv
+    3. bcryptjs: for saving passwords on db
+    4. express-async-handler: for better asynchronous handler
+5. Connect to MongoDB Atlas
+6. Use MongoDB instead of data.ts in Apis
--- backend/src/routers/food.router.ts
+++ backend/src/routers/food.router.ts
@@ -1,7 +1,21 @@
 import { Router } from "express";
 import { sample_foods, sample_tags } from "../data";
+import expressAsyncHandler from "express-async-handler";
+import { FoodModel } from "../models/food.model";
 
 const router = Router();
+
+router.get("/seed", expressAsyncHandler(
+    async (req, res) => {
+        const foodsCount = await FoodModel.countDocuments();
+        if (foodsCount > 0) {
+            res.send("Seed is already done!");
+            return;
+        }
+        await FoodModel.create(sample_foods);
+        res.send("Seed is done!");
+    }
+))
 
 router.get("/", (req, res) => {
     res.send(sample_foods);
